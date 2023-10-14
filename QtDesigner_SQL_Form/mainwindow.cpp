849dbc7cd6b499747af1ddc7dabc9c9e3ddf9f3c
Ilia Surikov <surikovis89@gmail.com>
2023-10-14 21:40:49 +0300

    Commit 1. SetStyleSheet using

--- QtDesigner_SQL_Form/mainwindow.cpp
+++ QtDesigner_SQL_Form/mainwindow.cpp
@@ -10,12 +10,34 @@ MainWindow::MainWindow(QWidget *parent, Qt::WindowFlags type)
     login_dialog = new LoginDialogSql();
 
 
-    QPalette win_pal (palette());
-    win_pal.setColor(QPalette::ColorRole::Window, QColor(49,93,239,200));
-    win_pal.setColor(QPalette::ColorRole::Button, QColor(255,255,255,255));
-    this->setPalette(win_pal);
-    ui->pb_exit->setPalette(win_pal);
-    ui->pb_login_sql->setPalette(win_pal);
+	//QPalette win_pal (palette());
+	auto win_pal2 = this->palette();
+	win_pal2.setColor(QPalette::ColorRole::Window, QColor(49,93,239,200));
+	//win_pal2.setColor(QPalette::ColorRole::Button, QColor(255,255,255,255));
+
+
+
+//	win_pal2.setBrush(QPalette::Button, QBrush(QColor(49,93,239,200), Qt::SolidPattern));
+//	win_pal2.setBrush(QPalette::Light, QBrush(QColor(49,93,239,200), Qt::SolidPattern));
+//	win_pal2.setBrush(QPalette::Midlight, QBrush(QColor(49,93,239,200), Qt::SolidPattern));
+//	win_pal2.setBrush(QPalette::Dark, QBrush(QColor(49,93,239,200), Qt::SolidPattern));
+//	win_pal2.setBrush(QPalette::Mid, QBrush(QColor(49,93,239,200), Qt::SolidPattern));
+//	win_pal2.setBrush(QPalette::Base, QBrush(QColor(49,93,239,200), Qt::SolidPattern));
+//	win_pal2.setBrush(QPalette::Shadow, QBrush(QColor(49,93,239,200), Qt::SolidPattern));
+//	win_pal2.setBrush(QPalette::Highlight, QBrush(QColor(49,93,239,200), Qt::SolidPattern));
+//	win_pal2.setBrush(QPalette::PlaceholderText, QBrush(QColor(49,93,239,200), Qt::SolidPattern));
+
+
+	//win_pal2.setBrush(QPalette::ButtonText, Qt::blue);
+	//win_pal2.setBrush(QPalette::Text, Qt::magenta);
+	//win_pal2.setBrush(QPalette::All, QPalette::Base, Qt::green);
+	//win_pal2.setBrush(QPalette::Window, QBrush(QColor(49,93,239,200), Qt::SolidPattern));
+
+	this->setPalette(win_pal2);
+	//ui->pb_exit->setPalette(win_pal2);
+	//ui->pb_login_sql->setPalette(win_pal2);
+	this->setStyleSheet("QMainWindow{background-color:;}");
+	this->setStyleSheet("QPushButton{background-color:white;} QPushButton:hover{background-color:red;} QPushButton:pressed {background-color:magenta; }");
 }
 
 MainWindow::~MainWindow()
